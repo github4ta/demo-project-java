1a75f64279c6e2c0ad3528961ff063de6a241b22
Sergei Tsarik <serge.tsarik@gmail.com>
2023-10-10 20:50:49 +0300

    refactor hello class

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
 class Hello {
-public void print() {
-  int b = 2;
-  int c = 3, d = 5;
-  double d2;
-  d2 = 5;
-}
+  public static void main(String[] args) {
+    System.out.println("Hello java");
+  }
 }
